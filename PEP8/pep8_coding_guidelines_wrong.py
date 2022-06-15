0ce7486ac3aaaff5f8ae94edd6f2d3cd25e73f81
Esuabom Dijemeni <e.dijemeni@gmail.com>
2022-06-15 15:51:35 +0100

    Completed PEP8 wrong file

---
+++ PEP8/pep8_coding_guidelines_wrong.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import sys 
+from my_lib import print_hello
+
+def MyFunctionWithManyParams(param1, param2 ,param3 ,param4 ,param5 ,param6 ,param7):
+    return "Hello world"
+
+user1 = "Jan"
+user2 = "Max"
+user3 = "Marcus"
+
+my_list = [user1, user2, user3]
+
+if __name__ == "__main__":
+    print_hello()
