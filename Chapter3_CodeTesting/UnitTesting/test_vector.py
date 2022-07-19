a17f8f9252e11164adb4cb099c58ac7c71e50bb3
Esuabom Dijemeni <e.dijemeni@gmail.com>
2022-07-19 17:08:22 +0100

    Started testing codebase for Vector class

---
+++ Chapter3_CodeTesting/UnitTesting/test_vector.py
@@ -0,0 +1,8 @@
+'''Test code
+'''
+import unittest
+
+from vector import Vector2D
+
+if __name__ == '__main__':
+    print('Hello')
---
+++ tests/test_vector.py
@@ -0,0 +1,9 @@
+'''Test vector.
+'''
+import unittest
+
+from fastvector import Vector2D
+
+
+if __name__ == '__main__':
+    unittest.main()
