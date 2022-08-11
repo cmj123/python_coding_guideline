e45bf74fc39b6d67b2d7728f593a4253523b3150
Esuabom Dijemeni <e.dijemeni@gmail.com>
2022-08-11 14:00:26 +0100

    Completed equality test case

--- Chapter3_CodeTesting/UnitTesting/test_vector.py
+++ Chapter3_CodeTesting/UnitTesting/test_vector.py
@@ -4,5 +4,22 @@ import unittest
 
 from vector import Vector2D
 
+class VectorTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.v1 = Vector2D(0,0)
+        self.v2 = Vector2D(-1,1)
+        self.v3 = Vector2D(2.5,-2.5)
+
+    def test_equality(self) -> None:
+        '''Tests the equality operator
+        '''
+        self.assertNotEqual(self.v1, self.v2)
+        expected_result = Vector2D(-1,1)
+        self.assertEqual(self.v2, expected_result)
+
+        
+
+from vector import Vector2D
+
 if __name__ == '__main__':
-    print('Hello')
+    unittest.main()
