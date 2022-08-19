bae05e95262b94acfe4b4f4a4a0ff011658290da
Esuabom Dijemeni <e.dijemeni@gmail.com>
2022-08-19 10:49:02 +0100

    Completed init test

--- Chapter4_Packages/html_doc/tests/test_vector.py
+++ Chapter4_Packages/html_doc/tests/test_vector.py
@@ -11,43 +11,49 @@ class VectorTests(unittest.TestCase):
         self.v2 = Vector2D(-1,1)
         self.v3 = Vector2D(2.5,-2.5)
 
-    def test_equality(self) -> None:
-        '''Tests the equality operator
-        '''
-        self.assertNotEqual(self.v1, self.v2)
-        expected_result = Vector2D(-1,1)
-        self.assertEqual(self.v2, expected_result)
+    def test_init(self) -> None:
+        result = Vector2D(-1, 1)
+        self.assertEqual(result, self.v2)
+        self.assertRaises(TypeError, Vector2D, 0, 'a')
+        self.assertRaises(TypeError, Vector2D, 'B', 1)
 
-    def test_add(self) -> None:
-        '''Test the addition operator
-        '''
-        result = self.v1 + self.v2
-        expected_result = Vector2D(-1,1)
-        self.assertEqual(result, expected_result)
+    # def test_equality(self) -> None:
+    #     '''Tests the equality operator
+    #     '''
+    #     self.assertNotEqual(self.v1, self.v2)
+    #     expected_result = Vector2D(-1,1)
+    #     self.assertEqual(self.v2, expected_result)
 
-    def test_sub(self):
-        '''Tests the subtraction operator
-        '''
-        result = self.v2 - self.v3
-        expected_result = Vector2D(-3.5, 3.5)
-        self.assertEqual(result, expected_result)
+    # def test_add(self) -> None:
+    #     '''Test the addition operator
+    #     '''
+    #     result = self.v1 + self.v2
+    #     expected_result = Vector2D(-1,1)
+    #     self.assertEqual(result, expected_result)
 
-    def test_mul(self):
-        '''Tests the multiplication operator
-        '''
-        result1 = self.v1 * 5
-        expected_result1 = Vector2D(0.0,0.0)
-        self.assertEqual(result1, expected_result1)
-        result2 = self.v1 * self.v2
-        expected_result2 = 0.0
-        self.assertEqual(result2, expected_result2)
+    # def test_sub(self):
+    #     '''Tests the subtraction operator
+    #     '''
+    #     result = self.v2 - self.v3
+    #     expected_result = Vector2D(-3.5, 3.5)
+    #     self.assertEqual(result, expected_result)
 
-    def test_div(self):
-        '''Tests the division operator
-        '''
-        result = self.v3 / 5 
-        expected_result = Vector2D(0.5, -0.5)
-        self.assertEqual(result, expected_result)
+    # def test_mul(self):
+    #     '''Tests the multiplication operator
+    #     '''
+    #     result1 = self.v1 * 5
+    #     expected_result1 = Vector2D(0.0,0.0)
+    #     self.assertEqual(result1, expected_result1)
+    #     result2 = self.v1 * self.v2
+    #     expected_result2 = 0.0
+    #     self.assertEqual(result2, expected_result2)
+
+    # def test_div(self):
+    #     '''Tests the division operator
+    #     '''
+    #     result = self.v3 / 5 
+    #     expected_result = Vector2D(0.5, -0.5)
+    #     self.assertEqual(result, expected_result)
 
 if __name__ == '__main__':
     unittest.main()
