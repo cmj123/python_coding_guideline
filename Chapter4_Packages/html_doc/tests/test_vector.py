8afe140050ad1ea912ddfbc88a16b601de91b600
Esuabom Dijemeni <e.dijemeni@gmail.com>
2022-08-22 15:50:12 +0100

    Code Coverage

--- Chapter4_Packages/html_doc/tests/test_vector.py
+++ Chapter4_Packages/html_doc/tests/test_vector.py
@@ -87,6 +87,7 @@ class VectorTests(unittest.TestCase):
         self.assertRaises(TypeError, Vector2D.check_vector_types, 1337)
         self.assertRaises(TypeError, Vector2D.check_vector_types, 13.37)
         self.assertRaises(TypeError, Vector2D.check_vector_types, '1337')
+        
 
     def test_str(self) -> None:
         self.assertEqual(str(self.v1), '(0, 0)')
